08b28d17d3b1f8db44aef5fe2d96937cb5933017
Sckende <clrccljuhasz@gmail.com>
2019-09-04 10:40:10 -0400

    Graphics with random effects

--- FOX_PAPER_glmm_VERSION_1.R
+++ FOX_PAPER_glmm_VERSION_1.R
@@ -4,6 +4,9 @@ list.files()
 data <- read.table("FOX_PAPER_Complete_database.txt", h = T, sep = "\t", dec = ".") # ***Climatic variables per 4hours observation***
 summary(data)
 
+# ****** #
+#data <- data[!data$YEAR %in% 2004:2005,]
+
 library("viridis") # For colors in plot
 library("lme4") # For generalised linear models
 library("glmmTMB")
@@ -15,6 +18,7 @@ library("car") # For the Anova command
 library("multcomp") # For the contrast analysis
 library("emmeans") # For the contrast analysis
 library("modEvA") # For the variance explained
+library("scales") # For the colour transparency
 
 # -------------------------------- #
 #### Correlation btw variables ####
@@ -50,12 +54,13 @@ data$fox.year <- paste(data$FOX.ID, data$YEAR, sep = "-")
 data$lmg.crash[data$lmg.year == "crash"] <- "crash"
 data$lmg.crash[data$lmg.year %in% c("inter", "peak")] <- "noCrash"
 
-#data <- data[!data$YEAR %in% 2004:2005,]
+# Creation of the variable for the offset
+data$log.obs <- log(data$OBS.LENGTH)
 
 # Full model
 pglmm <- glmer(AD.atq.number ~ scale(prec) + scale(max.temp) + I(scale(max.temp)^2) + scale(max.wind) + lmg.crash + scale(nest.dens)
                  + (1|fox.year)
-                 + offset(log(OBS.LENGTH)),
+                 + offset(log.obs),
              family = poisson(),
              #method = "REML",
              #select = TRUE,
@@ -78,7 +83,7 @@ hist(predict(pglmm, type = "response"), breaks = 0:50)
 # ------------------------------------- #
 
 pglm <- glm(AD.atq.number ~ prec + I(max.temp) + max.wind + lmg.crash + nest.dens
-            + offset(log(OBS.LENGTH)),
+            + offset(log.obs),
             family = poisson(),
             #method = "REML",
             #select = TRUE,
@@ -91,16 +96,16 @@ anova(pglmm, pglm) # DOESN'T WORK
 mod <- list()
 mod[[1]] <- glmer(AD.atq.number ~ scale(prec) + scale(max.temp) + I(scale(max.temp)^2) + scale(max.wind) + lmg.crash + scale(nest.dens)
                + (1|fox.year)
-               + offset(log(OBS.LENGTH)),
+               + offset(log.obs),
                family = poisson(),
                #method = "REML",
                #select = TRUE,
                data = data)
-summary
+summary(mod[[1]])
 
 mod[[2]] <- glmer(AD.atq.number ~ scale(prec) + scale(max.temp) + I(scale(max.temp)^2) + scale(max.wind) + scale(nest.dens)
                   + (1|fox.year)
-                  + offset(log(OBS.LENGTH)),
+                  + offset(log.obs),
                   family = poisson(),
                   #method = "REML",
                   #select = TRUE,
@@ -109,7 +114,7 @@ summary(mod[[2]])
 
 mod[[3]] <- glmer(AD.atq.number ~ scale(prec) + scale(max.temp) + scale(max.wind) + scale(nest.dens)
                   + (1|fox.year)
-                  + offset(log(OBS.LENGTH)),
+                  + offset(log.obs),
                   family = poisson(),
                   #method = "REML",
                   #select = TRUE,
@@ -118,7 +123,7 @@ summary(mod[[3]])
 
 mod[[4]] <- glmer(AD.atq.number ~ scale(prec) + scale(max.temp) + scale(nest.dens)
                   + (1|fox.year)
-                  + offset(log(OBS.LENGTH)),
+                  + offset(log.obs),
                   family = poisson(),
                   #method = "REML",
                   #select = TRUE,
@@ -137,7 +142,7 @@ hist(predict(mod[[4]], type = "response"), breaks = 0:50)
 
 mod[[5]] <- glmer(AD.atq.number ~ 1
                   + (1|fox.year)
-                  + offset(log(OBS.LENGTH)),
+                  + offset(log.obs),
                   family = poisson(),
                   #method = "REML",
                   #select = TRUE,
@@ -153,7 +158,7 @@ summary(mod[[5]])
 range(data$max.temp) # 0.595 - 15.351
 
 v <- seq(0, 16, by = 0.01)
-newdat <- data.frame(max.temp = v, prec = mean(data$prec), nest.dens = mean(data$nest.dens), OBS.LENGTH = mean(data$OBS.LENGTH))
+newdat <- data.frame(max.temp = v, prec = mean(data$prec), nest.dens = mean(data$nest.dens), log.obs = mean(data$log.obs))
 
 p <- predict(mod[[4]], newdata = newdat, type = "response", re.form = NA)
 
@@ -164,7 +169,7 @@ p <- predict(mod[[4]], newdata = newdat, type = "response", re.form = NA)
 range(data$prec) # 0 - 20
 
 v1 <- seq(0, 20, by = 0.01)
-newdat1 <- data.frame(max.temp = mean(data$max.temp), prec = v1, nest.dens = mean(data$nest.dens), OBS.LENGTH = mean(data$OBS.LENGTH))
+newdat1 <- data.frame(max.temp = mean(data$max.temp), prec = v1, nest.dens = mean(data$nest.dens), log.obs = mean(data$log.obs))
 
 p1 <- predict(mod[[4]], newdata = newdat1, type = "response", re.form = NA)
 
@@ -176,16 +181,49 @@ p1 <- predict(mod[[4]], newdata = newdat1, type = "response", re.form = NA)
 range(data$nest.dens) # 0.42 - 9.26
 
 v2 <- seq(0, 10, by = 0.01)
-newdat2 <- data.frame(max.temp = mean(data$max.temp), prec = mean(data$prec), nest.dens = v2, OBS.LENGTH = mean(data$OBS.LENGTH))
+newdat2 <- data.frame(max.temp = mean(data$max.temp), prec = mean(data$prec), nest.dens = v2, log.obs = mean(data$log.obs))
 
 p2 <- predict(mod[[4]], newdata = newdat2, type = "response", re.form = NA)
 
 
 # GRAPHICS #
 # ------- #
-x11(); par(mfrow = c(1, 2))
-plot(v, p, ylim = c(0, 1.5), type = "l", bty = "n", lwd = 2, xlab = "Maximal temperature & cumulative prec", col = "darkorange3")
-lines(v1, p1, type = "l", lwd = 2, col = "cyan3")
-legend("topright", legend = c("maximal temp", "cumulative prec"), col = c("darkorange3", "cyan3"), bty = "n", lwd = 2)
 
-plot(v2, p2, ylim = c(0, 1.5), type = "l", lwd = 2, bty = "n", xlab = "Goose nest density")
+# Temperature effect...
+x11()
+plot(v, p, ylim = c(0, 7), type = "l", bty = "n", lwd = 2.5, xlab = "Maximal temperature", ylab = "Fox attack number per hour", col = "darkorange4")
+# ... & associated random effects 
+re <- unique(data$fox.year)
+for(i in re){
+  newdat2 <- data.frame(max.temp = v, prec = mean(data$prec), nest.dens = mean(data$nest.dens), log.obs = mean(data$log.obs), fox.year = i)
+  pp <- predict(mod[[4]], newdata = newdat2, type = "response")
+  lines(v, pp, type = "l", lwd = 1, col = alpha("darkorange", 0.25))
+}
+points(data$max.temp, 3600*data$AD.atq.rate,col = "darkorange4") # WARNINGS ! points are missing on the plot because their value is really high
+
+# Precipitation effects....
+x11()
+plot(v1, p1, type = "l", ylim = c(0, 4), lwd = 2, col = "cyan4", bty = "n", ylab = "Fox attack number per hour", xlab = "Cumulative precipitation (mm)")
+
+# ... & associated random effects
+re <- unique(data$fox.year)
+for(i in re){
+  newdat3 <- data.frame(max.temp = mean(data$max.temp), prec = v1, nest.dens = mean(data$nest.dens), log.obs = mean(data$log.obs), fox.year = i)
+  pp <- predict(mod[[4]], newdata = newdat3, type = "response")
+  lines(v1, pp, type = "l", lwd = 1, col = alpha("cyan", 0.25))
+}
+points(data$prec, 3600*data$AD.atq.rate, col = "cyan4") # WARNINGS ! points are missing on the plot because their value is really high
+
+
+# Nest density effect ...
+x11()
+plot(v2, p2, ylim = c(0, 6), type = "l", lwd = 2, bty = "n", xlab = "Goose nest density", col = "darkgreen", ylab = "Fox attack number per hour")
+
+# ... & associated random effects
+re <- unique(data$fox.year)
+for(i in re){
+  newdat4 <- data.frame(max.temp = mean(data$max.temp), prec = mean(data$prec), nest.dens = v2, log.obs = mean(data$log.obs), fox.year = i)
+  pp <- predict(mod[[4]], newdata = newdat4, type = "response")
+  lines(v2, pp, type = "l", lwd = 1, col = alpha("green", 0.25))
+}
+points(data$nest.dens, 3600*data$AD.atq.rate, col = "darkgreen") # WARNINGS ! points are missing on the plot because their value is really high
