360dce89838bbe17a151b64b0335e42905bdbb64
Sckende <clrccljuhasz@gmail.com>
2019-09-25 11:08:53 -0400

    New script for testing new GAMM

---
+++ FOX-GAM_VERSION_4.R
@@ -0,0 +1,49 @@
+rm(list = ls())
+setwd(dir = "C:/Users/HP_9470m/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - ANALYSES/R analysis/Data")
+list.files()
+data <- read.table("FOX_PAPER_Complete_database.txt", h = T, sep = "\t", dec = ".") # ***Climatic variables per 4hours observation***
+summary(data)
+
+# ****** #
+#data <- data[!data$YEAR %in% 2004:2005,]
+
+library("viridis") # For colors in plot
+library("lme4") # For generalised linear models
+library("glmmTMB")
+library("optimx")
+library("visreg") # Vizualisation of model effects
+library("DHARMa") # For simulations
+library("AICcmodavg") # For AIC comparison
+library("car") # For the Anova command
+library("multcomp") # For the contrast analysis
+library("emmeans") # For the contrast analysis
+library("modEvA") # For the variance explained
+library("scales") # For the colour transparency
+library("ggplot2")
+library("GGally") # correlation panels
+library("mgcv")
+
+# Creation of the random variable fox.year
+data$fox.year <- paste(data$FOX.ID, data$YEAR, sep = "-")
+data$fox.year <- as.factor(data$fox.year)
+
+# Creation of a lemming variable with only two levels
+data$lmg.crash[data$lmg.year == "crash"] <- "crash"
+data$lmg.crash[data$lmg.year %in% c("inter", "peak")] <- "noCrash"
+
+# Creation of the variable for the offset
+data$log.obs <- log(data$OBS.LENGTH)
+
+# WARNING - data_test contains a modified variable for "lmg.year"
+data_test <- data
+data_test$lmg.year <- as.character(data_test$lmg.year)
+data_test$lmg.year[data_test$lmg.abun < 1] <- "crash"
+
+data_test$lmg.crash[data_test$lmg.year == "crash"] <- "crash"
+
+data_test$lmg.year <- as.factor(data_test$lmg.year)
+data_test$lmg.crash <- as.factor(data_test$lmg.crash)
+
+summary(data_test)
+
+
