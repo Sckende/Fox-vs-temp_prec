0247620032cfcee228a38d2d39daded84ae798c9
Sckende <clrccljuhasz@gmail.com>
2019-09-26 14:42:45 -0400

    Suite - Building of gamm models

--- FOX-GAM_VERSION_4.R
+++ FOX-GAM_VERSION_4.R
@@ -23,6 +23,13 @@ library("ggplot2")
 library("GGally") # correlation panels
 library("mgcv")
 
+# ------------------------------- #
+#### Creation of the database ####
+# ------------------------------#
+
+# Keeping the observation more or equal than 3 min (180 s)
+data <- data[data$OBS.LENGTH >= 180,]
+
 # Creation of the random variable fox.year
 data$fox.year <- paste(data$FOX.ID, data$YEAR, sep = "-")
 data$fox.year <- as.factor(data$fox.year)
@@ -33,6 +40,9 @@ data$lmg.crash[data$lmg.year %in% c("inter", "peak")] <- "noCrash"
 
 # Creation of the variable for the offset
 data$log.obs <- log(data$OBS.LENGTH)
+
+# Log transformation of lemming abundance variable
+data$log.lmgAbun <- log(data$lmg.abun)
 
 # WARNING - data_test contains a modified variable for "lmg.year"
 data_test <- data
@@ -46,4 +56,63 @@ data_test$lmg.crash <- as.factor(data_test$lmg.crash)
 
 summary(data_test)
 
+# Scaled data
+scaleData <- apply(data_test[,c(2, 14, 17, 21, 22, 24, 28)], MARGIN = 2, scale)
 
+scaleData <- cbind(scaleData, data_test[, c(7, 8, 23, 25:27)])
+summary(scaleData)
+
+# ------------------------ #
+### the best glmm model ###
+# ---------------------- #
+control <- glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 2000000))
+foxGlmm <- glmer(AD.atq.number ~ prec*lmg.crash + max.temp*lmg.crash + nest.dens + DATE*lmg.crash
+                   + (1|fox.year)
+                   + offset(log.obs),
+                   family = poisson(),
+                   control = control,
+                   #method = "REML",
+                   #select = TRUE,
+                   data = scaleData)
+
+summary(foxGlmm)
+x11();
+par(mfrow = c(2, 2))
+visreg(foxGlmm, "max.temp", by = "lmg.crash", overlay = T)
+visreg(foxGlmm, "prec", by = "lmg.crash", overlay = T)
+visreg(foxGlmm, "nest.dens", by = "lmg.crash", overlay = T)
+visreg(foxGlmm, "DATE", by = "lmg.crash", overlay = T)
+# -------------------------- #
+### test with gamm models ###
+# ------------------------ #
+lmgGamm <- list()
+lmgGamm[[1]] <- gam(AD.atq.number ~ s(prec, by = lmg.crash, k = 1) + s(max.temp, by = lmg.crash, k = 1) + s(nest.dens, k = 1) + s(DATE, by = lmg.crash, k = 1) + s(fox.year, bs = "re") + offset(log.obs),
+         family = poisson(),
+         method = "REML", # Automatic selection of the lambda term (trade-off between likelihood and wiggliness)
+         #select = TRUE,
+         data = scaleData)
+summary(lmgGamm[[1]])
+ 
+plot(lmgGamm[[1]], page = 1, all.terms = TRUE)
+
+# ----- #
+lmgGamm[[2]] <- gam(AD.atq.number ~ s(prec, by = lmg.crash, k = 2) + s(max.temp, by = lmg.crash, k = 2) + s(nest.dens, k = 2) + s(DATE, by = lmg.crash, k = 2) + s(fox.year, bs = "re") + offset(log.obs),
+                    family = poisson(),
+                    method = "REML", # Automatic selection of the lambda term (trade-off between likelihood and wiggliness)
+                    #select = TRUE,
+                    data = scaleData)
+summary(lmgGamm[[2]])
+
+plot(lmgGamm[[2]], page = 1, all.terms = TRUE)
+
+# ----- #
+lmgGamm[[3]] <- gam(AD.atq.number ~ s(prec, by = lmg.crash, k = 3) + s(max.temp, by = lmg.crash, k = 3) + s(nest.dens, k = 3) + s(DATE, by = lmg.crash, k = 3) + s(fox.year, bs = "re") + offset(log.obs),
+                    family = poisson(),
+                    method = "REML", # Automatic selection of the lambda term (trade-off between likelihood and wiggliness)
+                    #select = TRUE,
+                    data = scaleData)
+summary(lmgGamm[[3]])
+
+plot(lmgGamm[[3]], page = 1, all.terms = TRUE)
+
+AIC(lmgGamm[[1]], lmgGamm[[2]], lmgGamm[[3]])
